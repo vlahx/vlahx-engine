beef4c61ceb2b684405234f3594a2a985cd4de88
VlahX Core Developer <admin@vlahx.org>
2026-09-01 06:36:34 +0100

    feat(entrypoint): unifiy run.sh with run.py to enable auto-reload and dynamic plugin routing on local venv

--- run.sh
+++ run.sh
@@ -10,4 +10,5 @@ fi
 . .venv/bin/activate
 pip install -r requirements.txt >/dev/null 2>&1 || true
 
-exec python -m uvicorn main:app --host 127.0.0.1 --port 8000
+# Rulăm același entrypoint Python ca și în Docker!
+exec python run.py
